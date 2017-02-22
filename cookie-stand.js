ee075a249dba6b87b3afd35a63e7c229b680403f
abodey <a.s.bodey@gmail.com>
2017-02-22 00:06:15 -0800

    add beging of function constructor

--- cookie-stand.js
+++ cookie-stand.js
@@ -1,27 +1,19 @@
 
 
-
-
-
-var pioneer{
- minCust :3,
- maxCust
- avgSales;
- function
+//set up custructor function
+function store(minC, maxC, avgC) {
+  this.minCust = minC;
+  this.maxCust = maxC;
+  this.avgCookie = avgC;
+  this.hourlySales = function getRandomArbitrary(minC, maxC) {
+    return Math.random() * (maxC - minC) + minC;
+  }
 };
 
-var portland{
 
-};
-
-var washington{
-
-}	;
-
-var sellwood	{
-
-};
-
-var pearl {
-
-};
+// feed in new stores
+var pioneer = new store(17, 88, 5.2);
+var portland = new store(6, 24, 1.2);
+var washington = new store(11, 38, 1.9);
+var sellwood = new store(20, 48, 3.3);
+var pearl = new store(3, 24, 2.6);
